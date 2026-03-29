df601bf2a9092694327882561c9e78ef6325298e
sahaya aa <sahayaaalayaicloud.com@sahayas-MacBook-Air.local>
2026-03-29 23:26:55 +0530

    commit

--- aalaya.c
+++ aalaya.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+#include<string.h>
 
+int main() {
+    printf("HEART WARNING GREETS\n");
+    printf("please enter your details\n");
+    
+    int age;
+    char name[50];
+    
+    printf("enter patients name : ");
+    scanf("%s", name);
+    
+    printf("enter patients age : ");
+    scanf("%d", &age);
+    
+    char patients_disease[50];
+    printf("enter patients disease : ");
+    scanf("%s", patients_disease);
+    
+    if(strcmp(patients_disease, "fever") == 0) {
+        printf("fever tablet cost = 500\n");
+    } else if(strcmp(patients_disease, "dolo") == 0) {
+        printf("dolo tablet cost = 550\n");
+    } else {
+        printf("vicks tablet cost = 50\n");
+    }
+    
+    return 0;
+}
